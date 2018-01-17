2f630bec77a7ebcb4a43210bc208d697d8690667
Marta Piecko <martapiecko@wp.pl>
2018-01-17 17:41:02 +0100

    Modified HomeActivity.java.

--- aplikacja-android/app/src/main/java/wisielec/wisielec/com/activity/HomeActivity.java
+++ aplikacja-android/app/src/main/java/wisielec/wisielec/com/activity/HomeActivity.java
@@ -1,6 +1,8 @@
 package wisielec.wisielec.com.activity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import wisielec.wisielec.com.R;
 
 /**
@@ -9,10 +11,28 @@ import wisielec.wisielec.com.R;
 
 public class HomeActivity extends MainActivity {
 
+    Button buttonLogin;
+    Button buttonRegistration;
+    Button buttonExit;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        buttonLogin = (Button) findViewById(R.id.buttonLogin);
+        buttonRegistration = (Button) findViewById(R.id.buttonRegistration);
+        buttonExit = (Button) findViewById(R.id.buttonExit);
+        OnClickListeners();
+    }
+
+    protected void OnClickListeners() {
+        final View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        };
     }
 
 }
