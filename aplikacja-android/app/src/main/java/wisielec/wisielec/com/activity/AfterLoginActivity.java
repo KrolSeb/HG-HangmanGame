2f2dcfb91195a4a7265575473139a4f6b6467576
krols <krolsebastian8@gmail.com>
2018-03-10 16:24:48 +0100

    Added method to class UserRepository.java, which retrieve logged user data from database. Changed content in class AfterLoginActivity.java, where methods pass received data about user to screen. Added internet permission to AndroidManifest.xml

--- aplikacja-android/app/src/main/java/wisielec/wisielec/com/activity/AfterLoginActivity.java
+++ aplikacja-android/app/src/main/java/wisielec/wisielec/com/activity/AfterLoginActivity.java
@@ -1,17 +1,21 @@
 package wisielec.wisielec.com.activity;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.view.View;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import com.squareup.picasso.Picasso;
+
 import wisielec.wisielec.com.R;
+import wisielec.wisielec.com.domain.User;
+import static wisielec.wisielec.com.repository.UserRepository.retrieveUserFromDatabase;
 
 public class AfterLoginActivity extends MainActivity {
     protected ImageView avatar;
@@ -21,6 +25,7 @@ public class AfterLoginActivity extends MainActivity {
     protected TextView pointsAmount;
     protected Button settingsButton;
     protected Button logoutButton;
+    protected Button reloadButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,9 +54,12 @@ public class AfterLoginActivity extends MainActivity {
         pointsAmount = findViewById(R.id.pointsAmountTextView);
         settingsButton = findViewById(R.id.settingsButton);
         logoutButton = findViewById(R.id.logoutButton);
+        reloadButton = findViewById(R.id.reloadButton);
 
         onSettingsButtonClick();
         onLogoutButtonClick();
+        directDataToTextViews();
+
     }
 
     private void onSettingsButtonClick() {
@@ -72,12 +80,50 @@ public class AfterLoginActivity extends MainActivity {
         });
     }
 
+    private void directDataToTextViews() {
+        reloadButton.setSoundEffectsEnabled(false);
+        reloadButton.setVisibility(View.GONE);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                reloadButton.performClick();
+            }
+        }, 1000);
+        getUserDataFromDatabase();
+    }
+
+
+    private void getUserDataFromDatabase() {
+        final User retrievedUser = retrieveUserFromDatabase();
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()) {
+                    case R.id.reloadButton:
+                        //Biblioteka do prostego dodawania obrazka z URL
+                        Picasso.get().load(retrievedUser.getAvatarURL()).into(avatar);
+
+                        username.setText(retrievedUser.getUsername());
+                        rankingPosition.setText(Integer.toString(retrievedUser.getRankingPosition()));
+                        rankLevel.setText(retrievedUser.getRank());
+                        pointsAmount.setText(Long.toString(retrievedUser.getPoints()));
+                        break;
+                    default:
+                        break;
+                }
+            }
+        };
+        reloadButton.setOnClickListener(listener);
+    }
+
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
-        } else {
+        }
+        else {
             super.onBackPressed();
         }
     }
--- aplikacja-android/app/src/main/java/wisielec/wisielec/com/repository/UserRepository.java
+++ aplikacja-android/app/src/main/java/wisielec/wisielec/com/repository/UserRepository.java
@@ -1,8 +1,53 @@
 package wisielec.wisielec.com.repository;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import wisielec.wisielec.com.domain.User;
+
 /**
- * Created by X on 2018-01-17.
+ * Created by Sebastian on 2018-01-17.
  */
 
-public class UserRepository {
+public class UserRepository{
+    protected static User receivedUser;
+    public static User retrieveUserFromDatabase(){
+        receivedUser = new User();
+        DatabaseReference mDatabaseStatic = FirebaseDatabase.getInstance().getReference("users");
+        Query queryRef = mDatabaseStatic.orderByChild("actuallyLogged").equalTo(true);
+        queryRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                receivedUser.setAvatarURL(dataSnapshot.getValue(User.class).getAvatarURL());
+                receivedUser.setUsername(dataSnapshot.getValue(User.class).getUsername());
+                receivedUser.setRankingPosition(dataSnapshot.getValue(User.class).getRankingPosition());
+                receivedUser.setRank(dataSnapshot.getValue(User.class).getRank());
+                receivedUser.setPoints(dataSnapshot.getValue(User.class).getPoints());
+            }
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+                receivedUser.setAvatarURL(dataSnapshot.getValue(User.class).getAvatarURL());
+                receivedUser.setUsername(dataSnapshot.getValue(User.class).getUsername());
+                receivedUser.setRankingPosition(dataSnapshot.getValue(User.class).getRankingPosition());
+                receivedUser.setRank(dataSnapshot.getValue(User.class).getRank());
+                receivedUser.setPoints(dataSnapshot.getValue(User.class).getPoints());
+            }
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {}
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {}
+            @Override
+            public void onCancelled(DatabaseError databaseError) {}
+        });
+        return receivedUser;
+    }
+
+    public void addUserToDatabase(User user){
+        DatabaseReference mDatabase;
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        mDatabase.child("users").push().setValue(user);
+    }
 }
