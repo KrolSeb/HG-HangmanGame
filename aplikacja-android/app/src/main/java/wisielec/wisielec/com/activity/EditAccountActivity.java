5ef3dd901bc34055cc0aa4e66e12731ce7240950
krols <krolsebastian8@gmail.com>
2018-05-17 11:37:26 +0200

    Added frontend for image gallery.

--- aplikacja-android/app/src/main/java/wisielec/wisielec/com/activity/EditAccountActivity.java
+++ aplikacja-android/app/src/main/java/wisielec/wisielec/com/activity/EditAccountActivity.java
@@ -1,11 +1,10 @@
 package wisielec.wisielec.com.activity;
 
 import android.app.AlertDialog;
-import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -32,8 +31,11 @@ public class EditAccountActivity extends MainActivity {
     protected Button saveButton;
     protected Button deleteAccountButton;
 
+    protected View viewDialog;
+    protected LayoutInflater inflater;
+
     private User user;
-    private Dialog dialog;
+    private AlertDialog alertDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,6 +49,8 @@ public class EditAccountActivity extends MainActivity {
         saveButton = findViewById(R.id.saveButton);
         deleteAccountButton = findViewById(R.id.deleteAccountButton);
 
+        inflater = LayoutInflater.from(this);
+
         Intent intent = getIntent();
         user = (User) intent.getSerializableExtra("user");
 
@@ -58,13 +62,45 @@ public class EditAccountActivity extends MainActivity {
         onClickListeners();
     }
 
+    private void getImagesFromURLs(){
+        ImageView imageView1 = viewDialog.findViewById(R.id.imageView1);
+        Picasso.get().load("https://image.ibb.co/eHX6dy/astronaut.jpg").into(imageView1);
+
+        ImageView imageView2 = viewDialog.findViewById(R.id.imageView2);
+        Picasso.get().load("https://image.ibb.co/hEExkd/cat.jpg").into(imageView2);
+
+        ImageView imageView3 = viewDialog.findViewById(R.id.imageView3);
+        Picasso.get().load("https://image.ibb.co/cbGA5d/dog.jpg").into(imageView3);
+
+        ImageView imageView4 = viewDialog.findViewById(R.id.imageView4);
+        Picasso.get().load("https://image.ibb.co/fD5Rdy/duck.jpg").into(imageView4);
+
+        ImageView imageView5 = viewDialog.findViewById(R.id.imageView5);
+        Picasso.get().load("https://image.ibb.co/c9gckd/fish.jpg").into(imageView5);
+
+        ImageView imageView6 = viewDialog.findViewById(R.id.imageView6);
+        Picasso.get().load("https://image.ibb.co/cQxTrJ/frog.jpg").into(imageView6);
+
+        ImageView imageView7 = viewDialog.findViewById(R.id.imageView7);
+        Picasso.get().load("https://image.ibb.co/fFUxkd/guitar.jpg").into(imageView7);
+
+        ImageView imageView8 = viewDialog.findViewById(R.id.imageView8);
+        Picasso.get().load("https://image.ibb.co/gJzxkd/palm_tree.jpg").into(imageView8);
+
+        ImageView imageView9 = viewDialog.findViewById(R.id.imageView9);
+        Picasso.get().load("https://image.ibb.co/iVwckd/skater.jpg").into(imageView9);
+    }
+
     protected void onClickListeners(){
         final View.OnClickListener listener = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 switch (view.getId()){
                     case R.id.uploadNewAvatarButton:
+                        viewDialog = inflater.inflate(R.layout.dialog_choose_avatar, null);
+                        getImagesFromURLs();
                         launchAvatarChoose();
+                        viewDialog = null;
                         break;
                     case R.id.saveButton:
                         confirmChanges();
@@ -97,12 +133,7 @@ public class EditAccountActivity extends MainActivity {
     }
 
     public void launchAvatarChoose(){
-        AlertDialog alertDialog = new AlertDialog.Builder(EditAccountActivity.this).setTitle("Wybierz avatar").setView(R.layout.dialog_choose_avatar).create();
+        alertDialog = new AlertDialog.Builder(EditAccountActivity.this).setTitle("Wybierz avatar").setView(viewDialog).create();
         alertDialog.show();
     }
-
-    private void updateAvatar(){
-
-    }
-
 }
