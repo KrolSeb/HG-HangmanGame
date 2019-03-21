df300dbcb73cb423033295e4e17d1cb831e5e50c
= <Sebastian.Krol@Live.umcs.edu.pl>
2019-03-21 07:30:03 +0100

    Added temporary version of best users ranking retrieve in RankingActivity.java.

--- app/src/main/java/wisielec/wisielec/com/activity/RankingActivity.java
+++ app/src/main/java/wisielec/wisielec/com/activity/RankingActivity.java
@@ -1,16 +1,29 @@
 package wisielec.wisielec.com.activity;
 
 import android.os.Bundle;
-import android.widget.ListView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+
+
+import java.util.ArrayList;
 import java.util.List;
 
-import butterknife.BindView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import butterknife.ButterKnife;
 import wisielec.wisielec.com.R;
-import wisielec.wisielec.com.adapters.RankingListAdapter;
 import wisielec.wisielec.com.domain.User;
+import wisielec.wisielec.com.services.RankService;
 import wisielec.wisielec.com.services.UserService;
 
 /**
@@ -20,63 +33,101 @@ import wisielec.wisielec.com.services.UserService;
 public class RankingActivity extends MainActivity {
     /**
      * RankingActivityElements
-     * TextViews
-     */
-    @BindView(R.id.firstUserPointsRankPositonTextView)
-    TextView firstUserPointsTextView;
-    @BindView(R.id.secondUserPointsRankPositionTextView)
-    TextView secondUserPointsTextView;
-    @BindView(R.id.thirdUserPointsRankPositionTextView)
-    TextView thirdUserPointsTextView;
-    @BindView(R.id.firstUserNameRankPositionTextView)
-    TextView firstUserNameTextView;
-    @BindView(R.id.secondUserNameRankPositionTextView)
-    TextView secondUserNameTextView;
-    @BindView(R.id.thirdUserNameRankPositionTextView)
-    TextView thirdUserNameTextView;
-
-    /**
-     * RankingActivityElements
      * ListView
      */
-    @BindView(R.id.infinityUserRankingList)
-    ListView infinityUserRankingList;
+    private RecyclerView recyclerView;
+    private LinearLayoutManager linearLayoutManager;
+    private FirebaseRecyclerAdapter adapter;
 
-    private UserService userService = UserService.getInstance();
+
+
+    private UserService userService;
+    private RankService rankService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ranking);
-
         ButterKnife.bind(this);
 
-        final RankingListAdapter rankingListAdapter = new RankingListAdapter(this);
+        userService = UserService.getInstance();
+        rankService = RankService.getInstance();
 
-        userService.getBestUsersFromRanking(new UserService.IBestUserCallback() {
-            @Override
-            public void onSuccess(List<User> userList) {
-                User user;
-                String userName;
-
-                user = userList.get(0);
-                userName = (user.getUserName().isEmpty()) ? user.getEmail() : user.getUserName();
-                firstUserNameTextView.setText(String.valueOf(userName));
-                firstUserPointsTextView.setText(String.valueOf(user.getPoints()));
-
-                user = userList.get(1);
-                userName = (user.getUserName().isEmpty()) ? user.getEmail() : user.getUserName();
-                secondUserNameTextView.setText(String.valueOf(userName));
-                secondUserPointsTextView.setText(String.valueOf(user.getPoints()));
-
-                user = userList.get(2);
-                userName = (user.getUserName().isEmpty()) ? user.getEmail() : user.getUserName();
-                thirdUserNameTextView.setText(String.valueOf(userName));
-                thirdUserPointsTextView.setText(String.valueOf(user.getPoints()));
-
-                rankingListAdapter.setUserList(userList);
-                infinityUserRankingList.setAdapter(rankingListAdapter);
-            }
-        });
+        recyclerView = findViewById(R.id.list);
+        linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager.setReverseLayout(true);
+        linearLayoutManager.setStackFromEnd(true);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setHasFixedSize(true);
+        fetch();
     }
+
+    private void fetch() {
+        Query query = FirebaseDatabase.getInstance().getReference().child("users").orderByChild("points");
+        FirebaseRecyclerOptions<User> options = new FirebaseRecyclerOptions.Builder<User>()
+                        .setQuery(query, snapshot -> {
+                            User user = snapshot.getValue(User.class);
+                            return user;
+                        })
+                        .build();
+
+        adapter = new FirebaseRecyclerAdapter<User, ViewHolder>(options) {
+
+
+            @Override
+            public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext())
+                        .inflate(R.layout.list_item,parent, false);
+
+                return new ViewHolder(view);
+            }
+
+            @Override
+            protected void onBindViewHolder(@NonNull ViewHolder holder, int position, @NonNull User user) {
+                holder.setTxtTitle(String.valueOf(user.getUserName()));
+                holder.setTxtDesc(String.valueOf(user.getPoints()));
+                holder.root.setOnClickListener(view -> Toast.makeText(RankingActivity.this, String.valueOf(position), Toast.LENGTH_SHORT).show());
+            }
+
+        };
+        recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        adapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        adapter.stopListening();
+    }
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public LinearLayout root;
+        public TextView txtTitle;
+        public TextView txtDesc;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            root = itemView.findViewById(R.id.list_root);
+            txtTitle = itemView.findViewById(R.id.list_title);
+            txtDesc = itemView.findViewById(R.id.list_desc);
+        }
+
+
+
+        public void setTxtTitle(String string) {
+            txtTitle.setText(string);
+        }
+
+
+        public void setTxtDesc(String string) {
+            txtDesc.setText(string);
+        }
+    }
+
 }
