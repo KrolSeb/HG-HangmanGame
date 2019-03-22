315462343720eaedda402c7f368a392d6c5ca614
= <Sebastian.Krol@Live.umcs.edu.pl>
2019-03-22 10:03:31 +0100

    Updated SignUpActivity.java - removed cancel button.

--- app/src/main/java/wisielec/wisielec/com/activity/SignUpActivity.java
+++ app/src/main/java/wisielec/wisielec/com/activity/SignUpActivity.java
@@ -1,6 +1,5 @@
 package wisielec.wisielec.com.activity;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -9,7 +8,6 @@ import android.widget.EditText;
 
 import wisielec.wisielec.com.R;
 import wisielec.wisielec.com.domain.User;
-import wisielec.wisielec.com.interfaces.Callback;
 import wisielec.wisielec.com.services.UserService;
 
 /**
@@ -18,57 +16,41 @@ import wisielec.wisielec.com.services.UserService;
 
 public class SignUpActivity extends AbstractAccessActivity {
     private static final String TAG = "SignUpActivity";
-    Button buttonCancel;
-    Button buttonRegistration;
-    EditText emailInput;
-    EditText passwordInput;
-    EditText repeatPasswordInput;
-
+    private Button buttonRegistration;
+    private EditText emailInput;
+    private EditText passwordInput;
+    private EditText repeatPasswordInput;
     private UserService userService = UserService.getInstance();
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
-        buttonCancel = (Button) findViewById(R.id.buttonCancel);
         buttonRegistration = (Button) findViewById(R.id.buttonRegistration);
         emailInput = (EditText) findViewById(R.id.emailInput);
         passwordInput = (EditText) findViewById(R.id.passwordInput);
         repeatPasswordInput = (EditText) findViewById(R.id.repeatPasswordInput);
-        OnClickListeners();
+
+        onClickListeners();
     }
 
-    protected void OnClickListeners() {
-        final View.OnClickListener listener = new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                switch (v.getId()) {
-                    case R.id.buttonRegistration:
-                        User user = new User(emailInput.getText().toString(),passwordInput.getText().toString());
-                        if(!(isFormValid(emailInput))) return;
-                        userService.registerNewUser(SignUpActivity.this, user, new Callback() {
-                            @Override
-                            public void event() {
-                                changeToAfterLoginActivity();
-                            }
-                        });
-                        break;
-                    case R.id.buttonCancel:
-                        Context contextCancel;
-                        contextCancel = getApplicationContext();
-                        Intent intentCancel = new Intent(contextCancel, HomeActivity.class);
-                        startActivity(intentCancel);
-                        break;
-
-                    default:
-                        break;
-                }
+    protected void onClickListeners() {
+        final View.OnClickListener listener = v -> {
+            switch (v.getId()) {
+                case R.id.buttonRegistration:
+                    User user = new User(emailInput.getText().toString(),passwordInput.getText().toString());
+                    if(!(isFormValid(emailInput))) {
+                        return;
+                    }
+                    userService.registerNewUser(SignUpActivity.this, user, this::changeToAfterLoginActivity);
+                    break;
+                default:
+                    break;
             }
         };
+
         buttonRegistration.setOnClickListener(listener);
-        buttonCancel.setOnClickListener(listener);
     }
 
     @Override
