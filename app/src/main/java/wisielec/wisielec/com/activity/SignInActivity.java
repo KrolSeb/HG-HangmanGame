5b4707326bdcbeac57ab900e15a1fd0e35ac47fb
= <Sebastian.Krol@Live.umcs.edu.pl>
2019-04-01 14:13:10 +0200

    Updated SignInActivity.java and layout for this activity(activity_sign_in): - moved hardcoded Strings into static finals, - changed login user code and added notification shows during logging process.

--- app/src/main/java/wisielec/wisielec/com/activity/SignInActivity.java
+++ app/src/main/java/wisielec/wisielec/com/activity/SignInActivity.java
@@ -1,72 +1,101 @@
 package wisielec.wisielec.com.activity;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.util.Objects;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import wisielec.wisielec.com.R;
 import wisielec.wisielec.com.domain.User;
-import wisielec.wisielec.com.interfaces.Callback;
+import wisielec.wisielec.com.interfaces.UserLoginCallback;
+import wisielec.wisielec.com.services.EmailValidator;
 import wisielec.wisielec.com.services.UserService;
 
-/**
- * Created by X on 2018-01-18.
- */
 
 public class SignInActivity extends AbstractAccessActivity {
+    private static final String MESSAGE_EMPTY_EMAIL_AND_PASSWORD_INPUT = "Brak podanego e-maila i hasła";
+    private static final String MESSAGE_EMPTY_EMAIL_INPUT = "Brak podanego e-maila";
+    private static final String MESSAGE_EMPTY_PASSWORD_INPUT = "Brak podanego hasła";
+    private static final String MESSAGE_SUCCESSFUL_LOGIN = "Pomyślnie zalogowano.";
 
-    Button buttonLogin;
-    Button buttonRegistration;
-    EditText emailInput;
-    EditText passwordInput;
-    private UserService userService = UserService.getInstance();
+    private UserService userService;
+    private EmailValidator emailValidator;
+
+    @BindView(R.id.emailInput)
+    protected TextInputEditText emailInput;
+    @BindView(R.id.passwordInput)
+    protected TextInputEditText passwordInput;
+    @BindView(R.id.buttonLogin)
+    protected Button buttonLogin;
+    @BindView(R.id.buttonRegistration)
+    protected Button buttonRegistration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+        ButterKnife.bind(this);
 
-        buttonLogin = (Button) findViewById(R.id.buttonLogin);
-        buttonRegistration = (Button) findViewById(R.id.buttonRegistration);
-        emailInput = (EditText) findViewById(R.id.emailInput);
-        passwordInput = (EditText) findViewById(R.id.passwordInput);
-        OnClickListeners();
+        userService = UserService.getInstance();
+        emailValidator = new EmailValidator();
     }
 
-    protected void OnClickListeners() {
-        final View.OnClickListener listener = new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                switch (v.getId()) {
-                    case R.id.buttonLogin:
-                        signIn();
-                        break;
-                    case R.id.buttonRegistration:
-                        Context contextRegistration;
-                        contextRegistration = getApplicationContext();
-                        Intent intentRegistration = new Intent(contextRegistration, SignUpActivity.class);
-                        startActivity(intentRegistration);
-                        break;
-                    default:
-                        break;
-                }
-            }
-        };
-        buttonLogin.setOnClickListener(listener);
-        buttonRegistration.setOnClickListener(listener);
+    @OnClick(R.id.buttonLogin)
+    public void onButtonLoginClick(){
+        signIn();
+    }
+
+    @OnClick(R.id.buttonRegistration)
+    public void onButtonRegistrationClick(){
+        Intent intentRegistration = new Intent(SignInActivity.this, SignUpActivity.class);
+        startActivity(intentRegistration);
     }
 
     public void signIn() {
-        if (!isFormValid(emailInput)) return;
-        userService.loginUser(SignInActivity.this, new User(emailInput.getText().toString(), passwordInput.getText().toString()), new Callback() {
-            @Override
-            public void event() {
-                changeToAfterLoginActivity();
-            }
-        });
+        String email = emailInput.getText().toString();
+        String password = passwordInput.getText().toString();
+
+        if (email.isEmpty() || password.isEmpty()){
+            showNotificationIfEmptyInput(email,password);
+        }
+        else if(emailValidator.validate(emailInput)) {
+            userService.loginUser(SignInActivity.this,
+                    new User(Objects.requireNonNull(emailInput.getText()).toString(), Objects.requireNonNull(passwordInput.getText()).toString()), new UserLoginCallback() {
+                        @Override
+                        public void onSuccess() {
+                            showToast(MESSAGE_SUCCESSFUL_LOGIN,Toast.LENGTH_SHORT);
+                            changeToAfterLoginActivity();
+                        }
+
+                        @Override
+                        public void onFailed(String loginFailedMessage) {
+                            showToast(loginFailedMessage,Toast.LENGTH_LONG);
+                        }
+                    });
+        }
+    }
+
+    private void showNotificationIfEmptyInput(String email, String password){
+        if(email.isEmpty() && password.isEmpty()){
+            showToast(MESSAGE_EMPTY_EMAIL_AND_PASSWORD_INPUT, Toast.LENGTH_SHORT);
+        }
+        else if (email.isEmpty()){
+            showToast(MESSAGE_EMPTY_EMAIL_INPUT, Toast.LENGTH_SHORT);
+        }
+        else if (password.isEmpty()) {
+            showToast(MESSAGE_EMPTY_PASSWORD_INPUT, Toast.LENGTH_SHORT);
+        }
+    }
+
+    private void showToast(String message,int length){
+        Toast.makeText(SignInActivity.this,message,length).show();
     }
 
     @Override
