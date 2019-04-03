e35136cbdc2639f9be7f051059ee7499b8f749e7
= <Sebastian.Krol@Live.umcs.edu.pl>
2019-04-03 16:14:50 +0200

    Updated EditAccountActivity.java: - moved hardcoded Strings into static finals, - added grantReadExternalStoragePermission method, - added disable and enable upload button in avatar change dialog, - added permission state(accepted or denied) in avatar change dialog, - updated delete account dialog operations.

--- app/src/main/java/wisielec/wisielec/com/activity/EditAccountActivity.java
+++ app/src/main/java/wisielec/wisielec/com/activity/EditAccountActivity.java
@@ -1,11 +1,14 @@
 package wisielec.wisielec.com.activity;
 
+import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.res.ColorStateList;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
@@ -25,7 +28,10 @@ import wisielec.wisielec.com.interfaces.UpdateUsernameCallback;
 import wisielec.wisielec.com.interfaces.UserRemoveCallback;
 import wisielec.wisielec.com.services.UserService;
 
-public class EditAccountActivity extends MainActivity {
+
+public class EditAccountActivity extends AbstractEditAccountActivity {
+    private static final String MESSAGE_DENIED_READ_EXTERNAL_STORAGE_PERMISSION = "Odmówiono przyznania uprawnień.";
+
     private static final String DIALOG_DELETE_ACCOUNT_HEADER_MESSAGE = "Czy na pewno?";
     private static final String DIALOG_DELETE_ACCOUNT_MAIN_MESSAGE = "Potwierdzenie operacji oznacza usunięcie konta użytkownika wraz z wszystkimi danymi. Czy chcesz kontynuować?";
     private static final String DIALOG_CONFIRM_ANSWER = "OK";
@@ -43,12 +49,25 @@ public class EditAccountActivity extends MainActivity {
     private static final String BUTTON_DISABLED_TEXT_COLOR = "#EAEAEA";
     private static final int PICK_IMAGE_REQUEST = 1;
 
-    @BindView(R.id.avatarImageView) ImageView avatarImageView;
-    @BindView(R.id.usernameTextView) TextView usernameTextView;
-    @BindView(R.id.newUsernameEditText) EditText newUsernameEditText;
-    @BindView(R.id.changeAvatarButton) Button changeAvatarButton;
-    @BindView(R.id.saveButton) Button saveButton;
-    @BindView(R.id.deleteAccountButton) Button deleteAccountButton;
+    private static final String INTENT_FILE_TYPES = "image/*";
+    private static final String INTENT_FILE_TYPE_JPEG = "image/jpeg";
+    private static final String INTENT_FILE_TYPE_PNG = "image/jpeg";
+    private static final String PERMISSION_STATE_KEY = "permissionState";
+    private String[] mimeTypes = {INTENT_FILE_TYPE_JPEG, INTENT_FILE_TYPE_PNG};
+
+
+    @BindView(R.id.avatarImageView)
+    ImageView avatarImageView;
+    @BindView(R.id.usernameTextView)
+    TextView usernameTextView;
+    @BindView(R.id.newUsernameEditText)
+    EditText newUsernameEditText;
+    @BindView(R.id.changeAvatarButton)
+    Button changeAvatarButton;
+    @BindView(R.id.saveButton)
+    Button saveButton;
+    @BindView(R.id.deleteAccountButton)
+    Button deleteAccountButton;
 
     private View dialogAvatarView;
     private AlertDialog alertDialogAvatar;
@@ -66,12 +85,13 @@ public class EditAccountActivity extends MainActivity {
         setContentView(R.layout.activity_edit_account);
         ButterKnife.bind(this);
 
+        grantReadExternalStoragePermission();
         updateLayoutTextViews();
     }
 
     private void updateLayoutTextViews() {
         userService.getUserData(user -> {
-            if(!user.getAvatarURL().isEmpty()) {
+            if (!user.getAvatarURL().isEmpty()) {
                 Picasso.get().load(user.getAvatarURL()).into(avatarImageView);
             }
             else {
@@ -89,32 +109,32 @@ public class EditAccountActivity extends MainActivity {
 
     private void updateUserName() {
         String username = newUsernameEditText.getText().toString();
-        if (!username.equals("")){
+        if (!username.equals("")) {
             userService.updateUserName(username, new UpdateUsernameCallback() {
                 @Override
                 public void onSuccess() {
-                    showToast(UPDATE_USERNAME_SUCCESS,Toast.LENGTH_SHORT);
+                    showToast(UPDATE_USERNAME_SUCCESS, Toast.LENGTH_SHORT);
                 }
 
                 @Override
                 public void onFailed() {
-                    showToast(UPDATE_USERNAME_FAILED,Toast.LENGTH_SHORT);
+                    showToast(UPDATE_USERNAME_FAILED, Toast.LENGTH_SHORT);
                 }
             });
         }
     }
 
     private void updateAvatar() {
-        if (imageURI != null){
+        if (imageURI != null) {
             userService.updateAvatar(imageURI, new UpdateUserAvatarCallback() {
                 @Override
                 public void onSuccess() {
-                    showToast(DIALOG_UPDATE_AVATAR_SUCCESS,Toast.LENGTH_SHORT);
+                    showToast(DIALOG_UPDATE_AVATAR_SUCCESS, Toast.LENGTH_SHORT);
                 }
 
                 @Override
                 public void onFailed() {
-                    showToast(DIALOG_UPDATE_AVATAR_FAILED,Toast.LENGTH_SHORT);
+                    showToast(DIALOG_UPDATE_AVATAR_FAILED, Toast.LENGTH_SHORT);
                 }
             });
         }
@@ -123,7 +143,7 @@ public class EditAccountActivity extends MainActivity {
     }
 
     @OnClick(R.id.changeAvatarButton)
-    protected void onChangeAvatarButtonClick(){
+    protected void onChangeAvatarButtonClick() {
         buildChangeAvatarDialog();
         disableUpdateButton();
         showCurrentAvatar();
@@ -135,9 +155,10 @@ public class EditAccountActivity extends MainActivity {
         dialogAvatarView = null;
     }
 
-    private void buildChangeAvatarDialog(){
+    @SuppressLint("InflateParams")
+    private void buildChangeAvatarDialog() {
         LayoutInflater avatarInflater = LayoutInflater.from(this);
-        dialogAvatarView = avatarInflater.inflate(R.layout.dialog_choose_avatar, null);
+        dialogAvatarView = avatarInflater.inflate(R.layout.dialog_choose_avatar,null);
 
         dialogImageView = dialogAvatarView.findViewById(R.id.dialogImageView);
         dialogUploadButton = dialogAvatarView.findViewById(R.id.uploadAvatar);
@@ -152,15 +173,27 @@ public class EditAccountActivity extends MainActivity {
         dialogUpdateButton.setEnabled(false);
     }
 
-    private void enableUpdateButton(){
+    private void disableUploadButton() {
+        dialogUploadButton.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor((BUTTON_DISABLED_BACKGROUND_COLOR))));
+        dialogUploadButton.setTextColor(Color.parseColor(BUTTON_DISABLED_TEXT_COLOR));
+        dialogUploadButton.setEnabled(false);
+    }
+
+    private void enableUpdateButton() {
         dialogUpdateButton.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor((BUTTON_ENABLED_BACKGROUND_COLOR))));
         dialogUpdateButton.setTextColor(Color.parseColor(BUTTON_ENABLED_TEXT_COLOR));
         dialogUpdateButton.setEnabled(true);
     }
 
-    private void showCurrentAvatar(){
+    private void enableUploadButton() {
+        dialogUploadButton.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor((BUTTON_ENABLED_BACKGROUND_COLOR))));
+        dialogUploadButton.setTextColor(Color.parseColor(BUTTON_ENABLED_TEXT_COLOR));
+        dialogUploadButton.setEnabled(true);
+    }
+
+    private void showCurrentAvatar() {
         userService.getUserData(user -> {
-            if(!user.getAvatarURL().isEmpty()) {
+            if (!user.getAvatarURL().isEmpty()) {
                 Picasso.get().load(user.getAvatarURL()).into(dialogImageView);
             }
             else {
@@ -169,18 +202,26 @@ public class EditAccountActivity extends MainActivity {
         });
     }
 
-    protected void onDialogUploadButtonClick(){
-        dialogUploadButton.setOnClickListener(v -> openFileChooser());
+    protected void onDialogUploadButtonClick() {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        boolean permission = prefs.getBoolean(PERMISSION_STATE_KEY, false);
+        if (permission) {
+            enableUploadButton();
+            dialogUploadButton.setOnClickListener(v -> openFileChooser());
+        }
+        else {
+            disableUploadButton();
+            showToast(MESSAGE_DENIED_READ_EXTERNAL_STORAGE_PERMISSION, Toast.LENGTH_LONG);
+        }
     }
 
-    protected void onDialogUpdateButtonClick(){
+    protected void onDialogUpdateButtonClick() {
         dialogUpdateButton.setOnClickListener(v -> updateAvatar());
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
         if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
             imageURI = data.getData();
             Picasso.get().load(imageURI).into(dialogImageView);
@@ -189,16 +230,16 @@ public class EditAccountActivity extends MainActivity {
     }
 
     private void openFileChooser() {
-        String[] mimeTypes = {"image/jpeg", "image/png"};
         Intent intent = new Intent();
-        intent.setType("image/*");
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType(INTENT_FILE_TYPES);
         intent.putExtra(Intent.EXTRA_MIME_TYPES, mimeTypes);
         intent.setAction(Intent.ACTION_GET_CONTENT);
         startActivityForResult(intent,PICK_IMAGE_REQUEST);
     }
 
-    private void showToast(String message,int length){
-        Toast.makeText(EditAccountActivity.this,message,length).show();
+    private void showToast(String message, int length) {
+        Toast.makeText(EditAccountActivity.this, message, length).show();
     }
 
     @OnClick(R.id.deleteAccountButton)
@@ -211,17 +252,18 @@ public class EditAccountActivity extends MainActivity {
         deleteDialogBuilder.setTitle(DIALOG_DELETE_ACCOUNT_HEADER_MESSAGE);
         deleteDialogBuilder.setMessage(DIALOG_DELETE_ACCOUNT_MAIN_MESSAGE);
 
+        prepareIntentAfterRemoveAccount();
         deleteDialogBuilder.setPositiveButton(DIALOG_CONFIRM_ANSWER, (dialog, which) -> userService.removeUserAccount(new UserRemoveCallback() {
             @Override
             public void onSuccess() {
-                showToast(DELETE_ACCOUNT_SUCCESS,Toast.LENGTH_SHORT);
-                userService.logOut(() -> finish());
-                prepareIntentAfterRemoveAccount();
                 startActivity(afterRemoveUserIntent);
+                userService.logOut(() -> finish());
+                showToast(DELETE_ACCOUNT_SUCCESS,Toast.LENGTH_LONG);
             }
+
             @Override
             public void onFailed() {
-                showToast(DELETE_ACCOUNT_FAILED,Toast.LENGTH_SHORT);
+                showToast(DELETE_ACCOUNT_FAILED,Toast.LENGTH_LONG);
             }
         }));
 
@@ -230,8 +272,8 @@ public class EditAccountActivity extends MainActivity {
         deleteDialog.show();
     }
 
-    private void prepareIntentAfterRemoveAccount(){
-        afterRemoveUserIntent = new Intent(EditAccountActivity.this,SignInActivity.class);
+    private void prepareIntentAfterRemoveAccount() {
+        afterRemoveUserIntent = new Intent(EditAccountActivity.this, SignUpActivity.class);
         afterRemoveUserIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         afterRemoveUserIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
     }
